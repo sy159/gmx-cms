e2446fe736dd3cc031def608dccbdde28964c8fd
zhuzhiqiang <snow@cqgynet.com>
2020-01-09 14:37:04 +0800

    12

--- mainsys/config.py
+++ mainsys/config.py
@@ -23,8 +23,8 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # 是否在用户关闭浏览器时过�
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.163.com'  # QQ邮箱SMTP服务器(邮箱需要开通SMTP服务)
 EMAIL_PORT = 25  # QQ邮箱SMTP服务端口
-EMAIL_HOST_USER = ''  # 我的邮箱帐号
+EMAIL_HOST_USER = '123@qq.com'  # 我的邮箱帐号
 EMAIL_HOST_PASSWORD = '163sqm'  # 授权码
-EMAIL_SUBJECT_PREFIX = ''  # 为邮件标题的前缀,默认是'[django]'
+EMAIL_SUBJECT_PREFIX = 'gmx'  # 为邮件标题的前缀,默认是'[django]'
 EMAIL_USE_TLS = True  # 开启安全链接
 DEFAULT_FROM_EMAIL = SERVER_EMAIL = EMAIL_HOST_USER  # 设置发件人
--- mainsys/run.sh
+++ mainsys/run.sh
@@ -2,3 +2,5 @@
 python3 manage.py makemigrations --empty appname
 python3 manage.py makemigrations
 python3 manage.py migrate
+# 收集静态文件
+python3 manage.py collectstatic
